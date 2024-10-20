700e721509f684ff22bba7be670fffbb6a57bec3
Abel Pauw <abelpauw@gmail.com>
2024-10-20 13:57:44 +0200

    Player idle/walk animation
    - Added the idle animation to the player
    
    - Added the walking animation to the player
    
    - Added a script so the player moves when given input
    
    - Added code to the player movement script that makes the player flip when moving the other way

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,47 @@
+using System;
 using UnityEngine;
-
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    private Rigidbody2D rb;
 
-    void Start()
+    private Rigidbody2D rb;
+    private float moveInput;
+    private bool isFacingRight = true;
+
+    Animator animator;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(horizontalInput, 0) * moveSpeed;
-
-        rb.MovePosition(rb.position + movement * Time.deltaTime);
+        if (moveInput > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (moveInput < 0 && isFacingRight)
+        {
+            Flip();
+        }
+    }
+    void FixedUpdate()
+    {
+        // Apply movement based on input
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+    }
+    void Flip()
+    {
+        // Flip the player by inverting the X scale
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
+
